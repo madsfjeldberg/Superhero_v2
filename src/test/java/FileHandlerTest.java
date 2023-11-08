9e7a49d54355d8950a0e8d9d7e1d8a28872658b7
madsfjeldberg <mafj0001@stud.kea.dk>
2023-11-08 12:04:37 +0100

    FileHandler saveList og loadList test færdig

--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -26,10 +26,9 @@ public class FileHandlerTest {
         list = new ArrayList<>();
         hero = new Superhero("Hero", "hero1", "testkræft", 1, "JA", 100);
         Superhero hero2 = new Superhero("Hero2", "hero2", "testkræft2", 2, "NEJ", 200);
-        //Superhero hero3 = new Superhero("Hero3", "hero3", "testkræft3", 3, "JA", 300);
         list.add(hero);
         list.add(hero2);
-        //list.add(hero3);
+
     }
     @AfterEach
     void teardown() {
