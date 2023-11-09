1c7da03e1b03bcb6043b93104e0179cd118a3496
madsfjeldberg <mafj0001@stud.kea.dk>
2023-11-09 11:00:18 +0100

    opdateret menu

--- src/main/java/domain/comparators/StrengthComparator.java
+++ src/main/java/domain/comparators/StrengthComparator.java
@@ -9,10 +9,6 @@ public class StrengthComparator implements Comparator<Superhero> {
     public int compare(Superhero o1, Superhero o2) {
         int hero1 = o1.getStrength();
         int hero2 = o2.getStrength();
-        if (hero1 < hero2) {
-            return -1;
-        } else if (hero1 > hero2) {
-            return 1;
-        } else return 0;
+        return Integer.compare(hero1, hero2);
     }
 }
--- src/main/java/domain/comparators/YearComparator.java
+++ src/main/java/domain/comparators/YearComparator.java
@@ -1,7 +1,6 @@
 package domain.comparators;
 
 import domain.Superhero;
-
 import java.util.Comparator;
 
 public class YearComparator implements Comparator<Superhero> {
@@ -10,12 +9,6 @@ public class YearComparator implements Comparator<Superhero> {
     public int compare(Superhero o1, Superhero o2) {
         int n1 = o1.getYearCreated();
         int n2 = o2.getYearCreated();
-        if (n1 < n2) {
-            return -1;
-        }
-        else if (n1 > n2) {
-            return 1;
-        }
-        else return 0;
+        return Integer.compare(n1, n2);
     }
 }
